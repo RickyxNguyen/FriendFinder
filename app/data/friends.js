e272e8edb54b5ba72feb7967cea8f1f755d25780
Ricky Nguyen <ricky.nguyen@me.com>
2018-11-15 15:24:03 -0600

    The Final One

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+//hold all th epeople who take the survey
+var friendList = [
+    {
+      name: 'Ricky Nguyen',
+      profilePic: 'https://avatars2.githubusercontent.com/u/36218532?s=460&v=4',
+      scores: [
+        "5",
+        "4",
+        "4",
+        "2",
+        "5",
+        "1",
+        "4",
+        "3",
+        "5",
+        "4"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
